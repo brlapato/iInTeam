d3ce54ad54b2a067e124f40ca077fa901b69b669
Brian Lapato <blapato@gmail.com>
2021-10-07 18:21:15 -0400

    Default to https connections

--- Frontend/src/app/app.constants.ts
+++ Frontend/src/app/app.constants.ts
@@ -1,3 +1,3 @@
-export const API_URL = "http://backend.hcky43.com:8080";
-export const HOME_URL = "http://iinteam.hcky43.com:8080";
+export const API_URL = "https://backend.hcky43.com:8080";
+export const HOME_URL = "https://iinteam.hcky43.com:8080";
 export const PLAYER_ID = "PlayerId"
