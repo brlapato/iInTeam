50bc25ea5f454be3b2440eaf1c5a63978a758c07
Brian Lapato <blapato@gmail.com>
2021-10-07 22:31:13 -0400

    Fix logout URL

--- Frontend/src/app/app.constants.ts
+++ Frontend/src/app/app.constants.ts
@@ -1,3 +1,3 @@
 export const API_URL = "https://backend.hcky43.com";
-export const HOME_URL = "https://iinteam.hcky43.com:8080";
+export const HOME_URL = "https://iinteam.hcky43.com";
 export const PLAYER_ID = "PlayerId"
