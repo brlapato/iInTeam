2208e1a6b12dc6f12d0eede28d3f1a76a6cc0a1d
Brian Lapato <blapato@gmail.com>
2021-12-22 23:39:50 -0500

    Collapsable functionality on statistic tables

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AuthHttpInterceptor, AuthModule } from '@auth0/auth0-angular';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -70,6 +71,7 @@ import { StatDisplayComponent } from './components/ui/stat-display/stat-display.
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
